7f03e408b8880284de99dd5921322ec9898f685a
Nahuel Leiva <nahuel.leiva@sidesys.com>
2023-03-15 19:35:18 -0300

    Deleting unnecessary files, separating configurations in Startup class, adding Serilog.

--- Program.cs
+++ Program.cs
@@ -3,41 +3,26 @@ using ContactTrackingSystem.AutoMapper;
 using ContactTrackingSystem.Contracts;
 using ContactTrackingSystem.Logic;
 using ContactTrackingSystem.Models;
+using ContactTrackingSystem.StringUtils;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Events;
 
 namespace ContactTrackingSystem
 {
     public class Program
     {
+        private const string DefaultCorsPolicyName = "Default";
+
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
-            builder.Services.AddControllersWithViews();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen(options =>
-            {
-                options.SwaggerDoc("v1", 
-                    new Microsoft.OpenApi.Models.OpenApiInfo { 
-                        Title = "Contact Tracking System", 
-                        Description = "Angular application with Swagger", 
-                        Version = "V1" 
-                    });
-            });
-            builder.Services.AddDbContext<ContactTrackingSystemDbContext>(op =>
-            {
-                op.UseSqlite(builder.Configuration.GetConnectionString("DefaultDatabase"));
-            });
-
-            var config = new MapperConfiguration(cfg => 
-            {
-                cfg.AddProfile(new AutoMapperProfile());
-            });
-            var mapper = config.CreateMapper();
-            builder.Services.AddSingleton(mapper);
-            builder.Services.AddTransient<ILogicManager, LogicManager>();
+            ConfigureServices(builder);
+            ConfigureDBContext(builder);
+            ConfigureSerilog(builder);
+            ConfigureAutoMapper(builder);
+            ConfigureCors(builder);
 
             var app = builder.Build();
 
@@ -53,7 +38,7 @@ namespace ContactTrackingSystem
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Test1 Api v1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ContactTrackingSystem API V1");
             });
             app.UseRouting();
 
@@ -65,6 +50,80 @@ namespace ContactTrackingSystem
             app.MapFallbackToFile("index.html");
 
             app.Run();
+        }
+
+        private static void ConfigureServices(WebApplicationBuilder builder)
+        {
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1",
+                    new Microsoft.OpenApi.Models.OpenApiInfo
+                    {
+                        Title = "Contact Tracking System",
+                        Description = "Angular application with Swagger",
+                        Version = "V1"
+                    });
+            });
+
+            builder.Services.AddTransient<ILogicManager, LogicManager>();
+        }
+
+        private static void ConfigureDBContext(WebApplicationBuilder builder)
+        {
+            // Adding DB Context
+            builder.Services.AddDbContext<ContactTrackingSystemDbContext>(op =>
+            {
+                op.UseSqlite(builder.Configuration.GetConnectionString("DefaultDatabase"));
+            });
+        }
+
+        private static void ConfigureSerilog(WebApplicationBuilder builder)
+        {
+            var logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Debug)
+                .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .WriteTo.Async(c => c.File("Logs/logs_" + DateTime.UtcNow.ToString("yyyyMMdd") + "_.txt"))
+                .CreateLogger();
+
+            // Adding Serilog
+            builder.Logging.ClearProviders();
+            builder.Logging.AddSerilog(logger);
+        }
+
+        private static void ConfigureAutoMapper(WebApplicationBuilder builder)
+        {
+            // Configure AutoMapper profile
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new AutoMapperProfile());
+            });
+            var mapper = config.CreateMapper();
+            builder.Services.AddSingleton(mapper);
+        }
+
+        private static void ConfigureCors(WebApplicationBuilder builder)
+        {
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(DefaultCorsPolicyName, b =>
+                {
+                        b.WithOrigins(
+                            builder.Configuration["App:CorsOrigins"]
+                                .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                                .Select(o => o.RemovePostFix("/"))
+                                .ToArray()
+                        )
+                        .SetIsOriginAllowedToAllowWildcardSubdomains()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials();
+                });
+            });
         }
     }
 }
